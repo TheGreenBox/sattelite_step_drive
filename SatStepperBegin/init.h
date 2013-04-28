5b31ac0c62089447353428d9b228b920d80fccd6
akindyakov <akindyakov@gmail.com>
2013-04-28 20:43:35 +0400

    particle carry out functional from Stepper/deviceInit module

---
+++ SatStepperBegin/init.h
@@ -0,0 +1,15 @@
+/* ========================================================
+ * Organization: The green box 
+ *   
+ * Project name: Satellite stepper drive
+ * File name: init.h
+ * Description: head of init processes
+ * ========================================================
+ */
+
+#ifndef _INIT__H_
+#define _INIT__H_
+
+void deviceInit();
+
+#endif //_INIT__H_
