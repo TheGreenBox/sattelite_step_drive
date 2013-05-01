ec5e6341367a7e149ccf7b85c4571b18956c72ed
akindyakov <akindyakov@gmail.com>
2013-05-01 18:51:29 +0400

    header generate file ver2

--- headCreate.sh
+++ headCreate.sh
@@ -2,6 +2,21 @@
 
 if [ -f "$1" ] ; then
     echo "file " "$1" " already exist"
+    
 else
-   touch  "$1"
+    touch  "$1"
+
+    
+    echo "/* ========================================================"  >> "$1"
+    echo " * Organization: The Green Box"                               >> "$1"
+    echo " * "                                                          >> "$1"             
+    echo " * Project name: Satellite stepper drive"                     >> "$1"            
+    echo " * File name: " "$1"                                          >> "$1"
+    echo " * Description:"                                              >> "$1"
+    echo " * Author: "                                                  >> "$1"
+        
+    git config --global user.name >> "$1"
+    
+    echo " * ========================================================"  >> "$1"
+    echo " */" >> "$1"
 fi 
