96544e300a311f8ccb547f46825d03b90e62115c
AKindyakov <akindyakov@gmail.com>
2013-05-02 00:04:00 +0400

    modify header script - now it create header for not empty file

--- headCreate.sh
+++ headCreate.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
+userName=$(git config --global user.name)
+
+header=$(echo "/* ========================================================\n"\
+" * Organization: The Green Box\n"\
+" * \n"\
+" * Project name: Satellite stepper drive\n"\
+" * File name: " "$1" "\n"\
+" * Description:\n"\
+" * Author: " "$userName" "\n"\
+" * ========================================================\n"\
+" */\n")
+
 if [ -f "$1" ] ; then
     echo "file " "$1" " already exist"
     
+    rez=$(grep -c "Organization: The Green Box" "$1")
+    
+    if [ $rez != 0 ]; then
+        echo "find correct header -- > exit"
+    else
+        echo "not find correct header -- > add.."
+        touch ".tempFile"
+        mv "$1" ".tempFile"
+        echo "$header" >> "$1"
+        cat ".tempFile" >> "$1"
+        rm ".tempFile"
+    fi 
 else
     touch  "$1"
-
-    
-    echo "/* ========================================================"  >> "$1"
-    echo " * Organization: The Green Box"                               >> "$1"
-    echo " * "                                                          >> "$1"             
-    echo " * Project name: Satellite stepper drive"                     >> "$1"            
-    echo " * File name: " "$1"                                          >> "$1"
-    echo " * Description:"                                              >> "$1"
-    echo " * Author: "                                                  >> "$1"
-        
-    git config --global user.name >> "$1"
-    
-    echo " * ========================================================"  >> "$1"
-    echo " */" >> "$1"
+    echo "$header" >> "$1"
 fi 
+
