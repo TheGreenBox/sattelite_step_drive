c08dae0204daeff328ab636131bf02571b8c1ba8
AKindyakov <akindyakov@gmail.com>
2013-05-02 00:13:01 +0400

    ad header for main #13

--- SatStepperBegin/main.c
+++ SatStepperBegin/main.c
@@ -1,7 +1,15 @@
-/*
- * main.c
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Satellite stepper drive
+ * File name:  main.c 
+ * Description:
+ * Author: SNikitin, AKindyakov 
+ * ========================================================
  */
-int main(void) {
+
+int main(void) 
+{
 	
 	return 0;
 }
