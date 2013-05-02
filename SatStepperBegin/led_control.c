e1be996d587f5fc8c521af934a5c1dd9ecb301f8
SergNikitin <snikitin@outlook.com>
2013-05-02 18:53:42 +0400

    led_control files created; added head descriptions; #16

---
+++ SatStepperBegin/led_control.c
@@ -0,0 +1,10 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Satellite stepper drive
+ * File name:  led_control.c 
+ * Description: Module for controlling STATUS LED, LED2, 
+ * LED3
+ * Author: SNikitin
+ * ========================================================
+ */
---
+++ SatStepperBegin/led_control.h
@@ -0,0 +1,9 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Satellite stepper drive
+ * File name:  led_control.h
+ * Description: header for led_control.h
+ * Author: SNikitin
+ * ========================================================
+ */
