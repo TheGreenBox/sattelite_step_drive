1bd5c89d2be0908d47d9cd07e358790024d7e518
SergNikitin <snikitin@outlook.com>
2013-05-03 20:18:39 +0400

    control_interrupt.h , control_interrupt.c added, linked together; head added to both files; #19

---
+++ SatStepperBegin/control_interrupt.c
@@ -0,0 +1,12 @@
+/* ========================================================
+ * Organization: The Green Box
+ * 
+ * Project name: Satellite stepper drive
+ * File name:  control_interrupt.c 
+ * Description: Module for control interrupt functions 
+ * Author: SNikitin
+ * ========================================================
+ */
+
+#include "control_interrupt.h"
+
---
+++ SatStepperBegin/control_interrupt.h
@@ -0,0 +1,15 @@
+/* ========================================================
+ * Organization: The Green Boxa
+ * 
+ * Project name: Satellite stepper drive
+ * File name:  control_interrupt.h
+ * Description: header for control_interrupt.c
+ * Author: SNikitin
+ * ========================================================
+ */ 
+
+#ifndef INTERRUPT_CONTROL_INCLUDED
+#define INTERRUPT_CONTROL_INCLUDED
+
+
+#endif              //INTERRUPT_CONTROL_INCLUDED
