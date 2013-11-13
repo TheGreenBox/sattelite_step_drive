180914e22a5f1a901c46d6c11aab537d34c806c9
AKindyakov <akindyakov@gmail.com>
2013-11-14 01:16:16 +0400

    few syntax fix #32

--- models/pwm_generator_with_angle_feedback/current_integral/graph_current.py
+++ models/pwm_generator_with_angle_feedback/current_integral/graph_current.py
@@ -1,41 +1,49 @@
+#!/bin/env python
+#-*- utf-8 -*-
 
 import sys
+import math
 
 PHASE_L_H   = 1.65 * pow(10, -3)
 PHASE_R_OHM = 0.77
 PWM_P = 12
 PWM_N = 0
-PWM_PERIOD = 4./(6.*pow(10,7)
+PWM_PERIOD = 4.0/(6.0*pow(10,7))
 
 def getActiveCurrent(step, varepsilon, fillCoeff):
     T = PHASE_L_H / PHASE_R_OHM
     sigma = T/PWM_PERIOD
-    
+    sigma = 1/sigma
+    test = 1 - math.exp( -sigma + sigma * fillCoeff )
     i_n = ( PWM_P / PHASE_R_OHM )                                               \
-            * ( 1 - ( math.exp( -sigma * varepsilon )/( 1 - math.exp(-sigma)) ) \
-                    * ( (1 - math.exp(-step*sigma) )                            \
-                              - math.exp( -(1 - fillCoeff) * sigma )            \
-                                    * ( 1 - math.exp(-step*sigma + sigma) )     \
-              )       )                                                         \
-        - ( PWM_N / PHASE_R_OHM )                                               \
-            * ( math.exp(-sigma) )( 1 - math.exp(-sigma) )                      \
-            * ( 1 - math.exp( -step * sigma + sigma ) )                         \
-            * ( 1 - math.exp( -sigma + sigma * fillCoeff ) )                    \
+            * ( 1                                                               \
+                - ( math.exp( -sigma * varepsilon )/( 1 - math.exp(-sigma)) )   \
+                    * ( (1 - math.exp(-step*sigma)                              \
+                        - math.exp( -(1 - fillCoeff) * sigma )                  \
+                            * (1 - math.exp(-step*sigma + sigma))               \
+                        )))                                                     \
+          - ( PWM_N / PHASE_R_OHM )                                             \
+                * (( math.exp(-sigma) )/( 1 - math.exp(-sigma)))                \
+                * ( 1 - math.exp( -step * sigma + sigma ) )                     \
+                * ( 1 - math.exp( -sigma + sigma * fillCoeff ) )
     
     return i_n
 
 def getPassiveCurrent(step, varepsilon, fillCoeff):
     T = PHASE_L_H / PHASE_R_OHM
     sigma = T/PWM_PERIOD
-    
+    sigma = 1/sigma
     i_n = ( PWM_P + PWM_N / PHASE_R_OHM )                                       \
             * 1 / ( 1 - math.exp(-sigma) )                                      \
             * (1 - math.exp(-sigma*fillCoeff))                                  \
             * (1 - math.exp(-step*sigma))                                       \
             * math.exp(-sigma*varepsilon + sigma * fillCoeff)                   \
         - ( PWM_N / PHASE_R_OHM )                                               \
-            * ( 1 - math.exp( -( step - 1 + varepsilon - fillCoeff ) sigma ) )  \
+            * ( 1 - math.exp( -( step - 1 + varepsilon - fillCoeff ) * sigma ) )  \
     
     return i_n
-print ( getActiveCurrent( 0, 0.3, fillCoeff=0.5) )
-print ( getPassiveCurrent(0, 0.6, fillCoeff=0.5) )
+
+
+print ( getActiveCurrent( 0, 0.3, 0.5) )
+print ( getPassiveCurrent(0, 0.6, 0.5) )
+
