f783ddb7252c319fa05c6bfb1a3e37d925c3e19f
AKindyakov <akindyakov@gmail.com>
2013-11-24 18:16:25 +0400

    add estimate second conmponent in current growing estimate

--- models/pwm_generator_with_angle_feedback/current_integral/current_grow_estimate.py
+++ models/pwm_generator_with_angle_feedback/current_integral/current_grow_estimate.py
@@ -20,16 +20,21 @@ def yyy(sg, zt):
     return ( math.exp(-sg) * ( (1 - math.exp(-2*sg*zt) ) / (1 - math.exp(-1*sg*zt)) ))
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 
-#sigma = np.linspace(0., 1., 100)
-#zeta  = np.linspace(0., 1., 100)
-#h     = np.linspace(0., 1., 100)
+sigma = np.arange( 40., 45., 0.1 )
+zeta  = np.arange( 0.01, 1.,  0.01 )
 
-sigma = [1, 2, 3]
-zeta  = [1, 3, 1]
-h     = [3, 2, 2]
+x, y = np.meshgrid(sigma, zeta) 
 
-ax.plot(sigma, zeta, h);
+zs = np.array( [ yyy(xi,yi) for xi,yi in zip(np.ravel(x), np.ravel(y))] )
+z = zs.reshape(x.shape)
 
+#ax.plot_surface( x,y,z, rstride=4, cstride=4 )
+ax.plot_wireframe( x,y,z, rstride=4, cstride=4 )
+
+ax.set_xlabel(r'$\sigma$')
+ax.set_ylabel(r'$\zeta$')
+ax.legend([r"$ e^{-\sigma} \frac{1 - e^{-2 \sigma \zeta}}{1 - e^{ -\sigma \zeta}}$"])
 plt.show()
+
