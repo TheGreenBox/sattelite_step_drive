2ed049660c2b2d1dffa31e0c391c3d53568d704c
SergNikitin <snikitin@outlook.com>
2014-03-22 18:53:39 +0400

    - placed macros to separate file to be able to use them in different modules

---
+++ firmware/SatStepperBegin/utils/macros.h
@@ -0,0 +1,25 @@
+/* ========================================================
+ * Organization: The Green Box
+ *
+ * Project name: Satellite stepper drive
+ * File name: macros.h
+ * Description: Useful macros used across the project
+ * ========================================================
+ */
+
+#ifndef MACROS_H
+
+#define CPU_CLOCK_SPEED 6.000L   // for 60 Mhz; 10.000L for a 100MHz CPU clock speed
+#define ADC_usDELAY     10000L
+#define DELAY_US(A)     DSP28x_usDelay(((((long double) A * 1000.0L)    \
+                        / (long double)CPU_CLOCK_SPEED) - 9.0L) / 5.0L)
+
+extern void DSP28x_usDelay(unsigned long Count);
+
+// To receive more interrupts from this PIE group, acknowledge this interrupt
+// Writing 0 to bits is ignored, so there is no point using OR operator
+#define ACKNOWLEDGE_ONE_MORE_INTERRUPT_FROM_GROUP(GROUP_ID) \
+    PieCtrlRegs.PIEACK.all = GROUP_ID
+
+#define MACROS_H
+#endif //MACROS_H
