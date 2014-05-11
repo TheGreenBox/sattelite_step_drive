77902694b44fb9549073727be4def7c30d7ed310
Sergey Nikitin <snikitin@outlook.com>
2014-05-11 18:22:40 +0400

    rms noise cal complete (not fully tested) #40

--- tools/adc_noise_tools/histogram_creator.py
+++ tools/adc_noise_tools/histogram_creator.py
@@ -1,8 +1,13 @@
-import matplotlib
-matplotlib.use('pdf')
+# import matplotlib
+# matplotlib.use('pdf')
 
+import math
 import matplotlib.pyplot as plotter
 from matplotlib.backends.backend_pdf import PdfPages
+
+from scipy.integrate import quad
+import numpy
+
 
 allLogFiles = ('adc_0.dat', 'adc_1.dat', 'adc_2.dat', 'adc_3.dat', 'adc_4.dat')
 
@@ -52,14 +57,14 @@ def plotAllLogsToPdf(resFileName):
 
 temp_sigma = 1
 def integrand(z):
-    exponent = ((-z) ** 2) / (2 * (temp_sigma ** 2))
+    exponent = -(z ** 2) / (2 * (temp_sigma ** 2))
     return math.exp(exponent)
 
 def calculateX0(P):
     lastIntegral = 0
-    for upperLimit in numpy.arrange(-5.0, 0, 0.001):
-        integral = 2 / (temp_sigma * math.sqrt(2 * math.pi)) * quad(integrand, -numpy.inf, upperLimit)
-        if lastIntegral < P < integral:
+    for upperLimit in numpy.linspace(-5.0, 0, 5 / 0.001):
+        integral = (2 / (temp_sigma * math.sqrt(2 * math.pi) ) ) * quad(integrand, -numpy.inf, upperLimit)[0]
+        if lastIntegral <= P < integral:
             return upperLimit
 
         lastIntegral = integral
@@ -67,11 +72,10 @@ def calculateX0(P):
     print('unable to find upper integration limit that matches given P')
 
 def calculateRmsNoiseFrom(filename):
-    from scipy.integrate import quad
-    import numpy
-
     histKeys, histHeights = formHistogramDataFor(filename)
     mainBinMissProbability = 1 - max(histHeights) / sum(histHeights)
     x0 = calculateX0(mainBinMissProbability)
-    print('x0 = ' + x0)
-    print('adc nose is ' + str(x0 / (-0.5)) + ' LSB')
+
+    print('miss probability is ' + str(mainBinMissProbability))
+    print('x0 = ' + str(x0))
+    print('sigma (rms nose) is ' + str(0.5 / (-x0)) + ' LSB')
