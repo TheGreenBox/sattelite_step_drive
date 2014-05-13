20ebb2b5dfca8a06de17bf3ff67fd04a01c7b232
SergNikitin <snikitin@outlook.com>
2014-05-13 18:31:44 +0400

    - added methods for alternative noise-free bits calculation based on expected value - divided code to separate modules for histogram printing and noise evaluation

--- tools/adc_noise_tools/histogram_creator.py
+++ tools/adc_noise_tools/histogram_creator.py
@@ -1,14 +1,10 @@
-import math
 import matplotlib.pyplot as plotter
 from matplotlib.backends.backend_pdf import PdfPages
-
-from scipy.integrate import quad
-import numpy
 
 allLogFiles = ('adc_0.dat', 'adc_1.dat', 'adc_2.dat', 'adc_3.dat', 'adc_4.dat')
 
 
-def formHistogramDataFor(filename):
+def extractData(filename):
     logLines = open(filename).readlines()
 
     histogram = {}
@@ -34,7 +30,7 @@ def formHistogramDataFor(filename):
 
 
 def plotHistogramOf(filename):
-    histKeys, histHeights = formHistogramDataFor(filename)
+    histKeys, histHeights = extractData(filename)
 
     plotter.bar(range(min(histKeys), max(histKeys) + 1), histHeights)
     plotter.xlabel('ADC value')
@@ -42,7 +38,7 @@ def plotHistogramOf(filename):
     plotter.title(filename.upper())
 
 
-def plotAllLogsToPdf(resFileName):
+def plotAllTo(resFileName):
     pp = PdfPages(resFileName + '.pdf')
     for filename in allLogFiles:
         plotHistogramOf(filename)
@@ -50,46 +46,3 @@ def plotAllLogsToPdf(resFileName):
         plotter.cla()
 
     pp.close()
-
-# read 'Using Histogram Techniques to Measure A/D Converter Noise'
-# at analog.com for math explanation
-
-temp_sigma = 1
-
-
-def integrand(z):
-    exponent = -(z ** 2) / (2 * (temp_sigma ** 2))
-    return math.exp(exponent)
-
-
-def calculateX0(P):
-    lastIntegral = 0
-    for upperLimit in numpy.linspace(-5.0, 0, 5 / 0.001):
-        integral = (2 / (temp_sigma * math.sqrt(2 * math.pi))) \
-            * quad(integrand, -numpy.inf, upperLimit)[0]
-
-        if lastIntegral <= P < integral:
-            return upperLimit
-
-        lastIntegral = integral
-
-    print('unable to find upper integration limit that matches given P')
-
-
-def calculateRmsNoiseFrom(filename):
-    histKeys, histHeights = formHistogramDataFor(filename)
-    mainBinMissProbability = 1 - max(histHeights) / sum(histHeights)
-    x0 = calculateX0(mainBinMissProbability)
-    sigma = 0.5 / (-x0)
-    return sigma
-
-
-ALL_ADC_BITS = 12
-
-
-def calculateNoiseFreeBitsFrom(filename):
-    rmsNoise = calculateRmsNoiseFrom(filename)
-    peakToPeakNoise = 6.6 * rmsNoise
-    noiseFreeBits = math.floor(math.log((2**ALL_ADC_BITS) / peakToPeakNoise, 2))
-
-    print('adc has ' + str(noiseFreeBits) + ' noise-free bits')
