b81d5f37bcd99d97225e921c50d38e246ec815bb
SergNikitin <snikitin@outlook.com>
2014-05-16 23:15:03 +0400

    clunky matlab code chunk added, will be redone #48

--- firmware/SatStepperBegin/control_modules/commutation_angle.c
+++ firmware/SatStepperBegin/control_modules/commutation_angle.c
@@ -1,14 +1,13 @@
 #include "../state.h"
 #include "../utils/macros.h"
 #include "../sensors/encoder/encoder.h"
-#include "../connection_types.h"
 #include "commutation_angle.h"
 
-static inline int64_t totalEncoderTicks() {
+static int64_t totalEncoderTicks() {
     return gState.encoder.raw * ENCODER_RANGE + gState.encoder.precise;
 }
 
-static inline int64_t currentReletivePos() {
+static int64_t currentReletivePos() {
     int64_t rawSensorDeviation  =   (gState.encoder.raw % MOTOR_REDUCTION)
                                     * ENCODER_RANGE;
     int64_t fullSensorDeviation =   rawSensorDeviation + gState.encoder.precise;
@@ -17,9 +16,22 @@ static inline int64_t currentReletivePos() {
     return rotorDevInOneRevol;
 }
 
-void nextAlgoStepByCommAngle() {
+uint_fast8_t nextAlgoStepByCommAngle() {
+    int_fast8_t nextState;
+
     int64_t currentRelativePos = currentReletivePos();
+    float fuzzyEngineStep = currentRelativePos
+                            / currentAlgo->engineStepDegree;
+
     int_fast8_t direction = (totalEncoderTicks() <= gState.setPoint.position)
                             ? 1 : -1;
-    float engineStep = engineConnection->engineStepDegree;
+
+    // if (direction > 0) {
+        // nextState = floor(fuzzyEngineStep + gState.currentCommAngle);
+    // }
+    // else {
+        // nextState = ceil(fuzzyEngineStep - gState.currentCommAngle);
+    // }
+
+    return nextState % currentAlgo->algoStepsNumber;
 }
