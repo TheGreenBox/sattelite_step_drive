53f001b93c1a133415777545cc3fccc32fce5e0c
akindyakov <akindyakov@gmail.com>
2014-05-17 21:37:10 +0400

    make step() and stop() for control_algo module api #49

--- firmware/SatStepperBegin/control_algo.c
+++ firmware/SatStepperBegin/control_algo.c
@@ -14,55 +14,37 @@
 #include "algo_types.h"
 #include "control_algo.h"
 
-#include "control_modules/commutation_angle.h"
-
-controlAlgoFlags ctrlFlags = {0};
-
-static uint_fast8_t getNextAlgoStepNum() {
-    int_fast8_t nextStep;
-
-    if (ctrlFlags.feedbackActive) {
-        // nextStep = nextAlgoStepByCommAngle();
-    }
-    else {
-        nextStep =  gState.stepTicker
-                    % currentAlgo->algoStepsNumber;
-        if (nextStep < 0) {
-            nextStep += currentAlgo->algoStepsNumber;
-        }
-    }
-
-    return (uint_fast8_t)nextStep;
+void stop() {
+    setADirection(0);
+    setBDirection(0);
+    setCoeff(0);
 }
 
-PhaseSigns getNextPhaseSigns() {
-    PhaseSigns phaseSigns;
+void step(int_fast8_t dist) {
+    gState.stepTicker += dist;
 
     if (currentAlgo == NULL) {
-        phaseSigns.A = 0;
-        phaseSigns.B = 0;
-        return phaseSigns;
+        return;
     }
 
-    uint_fast8_t nextStep = getNextAlgoStepNum();
+    int_fast8_t nextStep =  gState.stepTicker % currentAlgo->algoStepsNumber;
+    if (nextStep < 0) {
+        nextStep += currentAlgo->algoStepsNumber;
+    }
+    setADirection(currentAlgo->phaseA[nextStep]);
+    setBDirection(currentAlgo->phaseB[nextStep]);
 
-    phaseSigns.A = currentAlgo->phaseA[nextStep];
-    phaseSigns.B = currentAlgo->phaseB[nextStep];
-
-    return phaseSigns;
+    int_fast8_t nextPwmStep =  gState.stepTicker % currentAlgo->algoPwmStepsNumber;
+    if (nextPwmStep < 0) {
+        nextPwmStep += currentAlgo->algoPwmStepsNumber;
+    }
+    setCoeff(currentAlgo->pwmCoeff[nextPwmStep]);
 }
 
-// TODO: this mustn't be here, it will be done in current feedback module
-uint16_t getPwmDutyByStep() {
-    if (currentAlgo == NULL) {
-        return MAX_PWM_DUTY;
-    }
+void nextStep() {
+    step(1);
+}
 
-    uint_fast8_t nextPwmStep =  getNextAlgoStepNum()
-                                % currentAlgo->algoPwmStepsNumber;
-
-    uint16_t pwm = MAX_PWM_DUTY - gState.motorControl.pwmDutyCycle;
-    pwm *= currentAlgo->pwmCoeff[nextPwmStep];
-
-    return MAX_PWM_DUTY - pwm;
+void prevStep() {
+    step(-1);
 }
