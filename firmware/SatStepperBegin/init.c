4af2d50abe20ce1884aef112f999a95339ebee15
SergNikitin <snikitin@outlook.com>
2014-05-24 22:40:26 +0400

    corrected state init a bit #53

--- firmware/SatStepperBegin/init.c
+++ firmware/SatStepperBegin/init.c
@@ -541,9 +541,13 @@ static void motorControlInit() {
 void stateInit() {
     gState.stepTicker           = 0;
     gState.currentSpeed         = 0;
-    gState.currentCommAngle     = 1;
-    gState.setPoint.position    = 0;
+    gState.currentCommAngle     = 1300;
+    gState.setPoint.position    = 100000;
     gState.setPoint.speed       = 0;
+
+#ifdef DEBUG
+    gState.encoder.errors = 0;
+#endif
 
     motorControlInit();
 }
