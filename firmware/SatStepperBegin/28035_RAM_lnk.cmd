8e08483d3ef0df8b9c09075c5db65dc4d500abab
akindyakov <akindyakov@gmail.com>
2014-05-25 17:24:10 +0400

    Repaired FLASH build:
    * Add linker script for flash build SatStepperBegin/28035_FLASH_lnk.cmd
    * Cut from FLASH linker script IQMath lib, we dont need it yet
    * Fix search path in FLASH build
    * Repair code style in linkers scripts

--- firmware/SatStepperBegin/28035_RAM_lnk.cmd
+++ firmware/SatStepperBegin/28035_RAM_lnk.cmd
@@ -1,62 +1,62 @@
+/*==================================================================================*/
+/*  User specific Linker command file for running from RAM                          */
+/*==================================================================================*/
+/*  FILE:           28035_RAM_lnk.cmd                                               */
+/*                                                                                  */
+/*  Description:    Linker Command File For 28035 examples that run out of RAM      */
+/*                                                                                  */
+/*                  This ONLY includes all SARAM blocks on the 28035 device.        */
+/*                  This does not include flash or OTP.                             */
+/*                                                                                  */
+/*                  Keep in mind that L0,L1,L2, and L3 are protected by the code    */
+/*                  security module.                                                */
+/*                                                                                  */
+/*                  What this means is in most cases you will want to move to       */
+/*                  another memory map file which has more memory defined.          */
+/*                                                                                  */
+/*  Target:         TMS320F28035                                                    */
+/*                                                                                  */
+/*  Version:        1.00                                                            */
+/*                                                                                  */
+/*----------------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------------*/
+/* For Code Composer Studio V2.2 and later                                          */
+/*----------------------------------------------------------------------------------*/
+/* In addition to this memory linker command file,                                  */
+/* add the header linker command file directly to the project.                      */
+/* The header linker command file is required to link the                           */
+/* peripheral structures to the proper locations within                             */
+/* the memory map.                                                                  */
+/*                                                                                  */
+/* The header linker files are found in <base>\DSP2803x_headers\cmd                 */
+/*                                                                                  */
+/* For BIOS applications add:      DSP2803x_Headers_BIOS.cmd                        */
+/* For nonBIOS applications add:   DSP2803x_Headers_nonBIOS.cmd                     */
+/*----------------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------------*/
 /*
-// TI File $Revision: /main/2 $
-// Checkin $Date: February 20, 2009   15:34:01 $
-//###########################################################################
-//
-// FILE:    28035_RAM_lnk.cmd
-//
-// TITLE:   Linker Command File For 28035 examples that run out of RAM
-//
-//          This ONLY includes all SARAM blocks on the 28035 device.
-//          This does not include flash or OTP.
-//
-//          Keep in mind that L0,L1,L2, and L3 are protected by the code
-//          security module.
-//
-//          What this means is in most cases you will want to move to
-//          another memory map file which has more memory defined.
-//
-//###########################################################################
-// $TI Release: 2803x Internal Release 2 $
-// $Release Date: November 11, 2008 $
-//###########################################################################
-*/
+   For Code Composer Studio prior to V2.2
+   --------------------------------------
+   1) Use one of the following -l statements to include the
+   header linker command file in the project. The header linker
+   file is required to link the peripheral structures to the proper
+   locations within the memory map
 
-/* ======================================================
-// For Code Composer Studio V2.2 and later
-// ---------------------------------------
-// In addition to this memory linker command file,
-// add the header linker command file directly to the project.
-// The header linker command file is required to link the
-// peripheral structures to the proper locations within
-// the memory map.
-//
-// The header linker files are found in <base>\DSP2803x_headers\cmd
-//
-// For BIOS applications add:      DSP2803x_Headers_BIOS.cmd
-// For nonBIOS applications add:   DSP2803x_Headers_nonBIOS.cmd
-========================================================= */
+   Uncomment this line to include file only for non-BIOS applications
+   -l DSP2803x_Headers_nonBIOS.cmd
 
-/* ======================================================
-// For Code Composer Studio prior to V2.2
-// --------------------------------------
-// 1) Use one of the following -l statements to include the
-// header linker command file in the project. The header linker
-// file is required to link the peripheral structures to the proper
-// locations within the memory map                                    */
+   Uncomment this line to include file only for BIOS applications
+   -l DSP2803x_Headers_BIOS.cmd
 
-/* Uncomment this line to include file only for non-BIOS applications */
-/* -l DSP2803x_Headers_nonBIOS.cmd */
-
-/* Uncomment this line to include file only for BIOS applications */
-/* -l DSP2803x_Headers_BIOS.cmd */
-
-/* 2) In your project add the path to <base>\DSP2803x_headers\cmd to the
+   2) In your project add the path to <base>\DSP2803x_headers\cmd to the
    library search path under project->build options, linker tab,
    library search path (-i).
-/*========================================================= */
+*/
+/*----------------------------------------------------------------------------------*/
 
-/* Define the memory block start/length for the DSP2803x
+/* Define the memory block start/length for the F28035
    PAGE 0 will be used to organize program sections
    PAGE 1 will be used to organize data sections
 
@@ -68,8 +68,8 @@
          Doing so will result in corruption of program
          and/or data.
 
-         L0 block is mirrored - that is it
-         can be accessed in high memory or low memory.
+         L0 memory block is mirrored - that is
+         it can be accessed in high memory or low memory.
          For simplicity only one instance is used in this
          linker file.
 
@@ -86,9 +86,9 @@ PAGE 0 :
    RAMM0      : origin = 0x000050, length = 0x0003B0
    TEXT       : origin = 0x008000, length = 0x001000
    RESET      : origin = 0x3FFFC0, length = 0x000002
-//   IQTABLES   : origin = 0x3FE000, length = 0x000B50     /* IQ Math Tables in Boot ROM */
-//   IQTABLES2  : origin = 0x3FEB50, length = 0x00008C     /* IQ Math Tables in Boot ROM */
-//   IQTABLES3  : origin = 0x3FEBDC, length = 0x0000AA	 /* IQ Math Tables in Boot ROM */
+/* IQTABLES   : origin = 0x3FE000, length = 0x000B50 */  /* IQ Math Tables in Boot ROM */
+/* IQTABLES2  : origin = 0x3FEB50, length = 0x00008C */  /* IQ Math Tables in Boot ROM */
+/* IQTABLES3  : origin = 0x3FEBDC, length = 0x0000AA */  /* IQ Math Tables in Boot ROM */
 
    BOOTROM    : origin = 0x3FF27C, length = 0x000D44
 
@@ -112,17 +112,17 @@ SECTIONS
    .cinit           : > RAMM0,     PAGE = 0
    .pinit           : > RAMM0,     PAGE = 0
    .switch          : > RAMM0,     PAGE = 0
-   .reset           : > RESET,     PAGE = 0, TYPE = DSECT /* not used, */
+   .reset           : > RESET,     PAGE = 0, TYPE = DSECT /* not used */
 
    .stack           : > RAMM1,     PAGE = 1
    .ebss            : > RAML3,     PAGE = 1
    .econst          : > RAML3,     PAGE = 1
    .esysmem         : > RAML3,     PAGE = 1
 
-//  IQmath           : > TEXT,      PAGE = 0
-//  IQmathTables     : > IQTABLES,  PAGE = 0, TYPE = NOLOAD
+/* IQmath           : > TEXT,      PAGE = 0                */
+/* IQmathTables     : > IQTABLES,  PAGE = 0, TYPE = NOLOAD */
 
-  /* Uncomment the section below if calling the IQNexp() or IQexp()
+ /* Uncomment the section below if calling the IQNexp() or IQexp()
       functions from the IQMath.lib library in order to utilize the
       relevant IQ Math table in Boot ROM (This saves space and Boot ROM
       is 1 wait-state). If this section is not uncommented, IQmathTables2
@@ -155,8 +155,3 @@ SECTIONS
 
 }
 
-/*
-//===========================================================================
-// End of file.
-//===========================================================================
-*/
