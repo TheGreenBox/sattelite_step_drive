8ed5d53c58d4252b9a59a36e07fc4845cc23d17a
akindyakov <akindyakov@gmail.com>
2014-05-25 19:53:08 +0400

    Add encoder calibration function #54
    * Add new module elementary_motions.c
    * Add new function encoderCalibration()
    * Add function encoderCalibration() to main.c

---
+++ firmware/SatStepperBegin/elementary_motions.h
@@ -0,0 +1,18 @@
+/**
+ * Organization: The green box
+ * Project name: Satellite stepper drive
+ *
+ * @file    elementary_motions.c
+ * @brief   Header for pretty little simple algos for elemetnaty motions
+ *  [github issue #54](https://github.com/TheGreenBox/sattelite_step_drive/issues/54)
+ */
+
+#pragma once
+
+/**
+ *  @brief Move sensor shaft to index bit
+ *      no more of one complete revolution
+ */
+
+void encoderCalibrating();
+
